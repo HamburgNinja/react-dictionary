4a2244399443a91b6dd81fba55289d4717b86c65
HamburgNinja <nina.drescher@gmail.com>
2021-07-09 15:37:20 +0200

    added open source link

--- src/App.js
+++ src/App.js
@@ -6,6 +6,21 @@ function App() {
       <header className="App-header">
         <p>Dictionary</p>
       </header>
+      <a
+        href="https://github.com/HamburgNinja/react-dictionary"
+        target="_blank"
+        rel="noreferrer"
+      >
+        open source code
+      </a>{" "}
+      by{" "}
+      <a
+        href="https://github.com/HamburgNinja"
+        target="_blank"
+        rel="noreferrer"
+      >
+        HamburgNinja
+      </a>
     </div>
   );
 }
