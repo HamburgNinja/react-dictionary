defe5f6edd31a31e6bae2f9091221370b96a5216
HamburgNinja <nina.drescher@gmail.com>
2021-07-09 15:25:23 +0200

    installed react and added bootstrap

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import App from "./App";
+
+import "./index.css";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
